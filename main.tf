984d2277c3eec8305a5cc360fd64eb4e45370110
ngurah-bagus-trisna <ngurahbagusnb391@gmail.com>
2025-06-24 15:31:13 +0700

    infra: Add resources to deploy serverless python using AWS Lambda

--- main.tf
+++ main.tf
@@ -1,67 +1,3 @@
-resource "aws_iam_policy" "lambda-policy" {
-  name        = "lambdaPolicy"
-  path        = "/"
-  description = "Allow write to S3 and read System Parameter"
-
-  policy = jsonencode({
-    "Version" = "2012-10-17",
-    "Statement" = [
-      {
-        "Sid"    = "AllowPutObject",
-        "Effect" = "Allow",
-        "Action" = [
-          "s3:PutObject"
-        ],
-        "Resource" = [
-          "*"
-        ]
-      },
-      {
-        "Sid"    = "AllowGetParameterSSM",
-        "Effect" = "Allow",
-        "Action" = [
-          "ssm:GetParameter"
-        ],
-        "Resource" = [
-          "*"
-        ]
-      },
-      {
-        "Sid"    = "AllowGetSecretValue",
-        "Effect" = "Allow",
-        "Action" = [
-          "secretmanager:GetSecretValue"
-        ],
-        "Resource" = [
-          "*"
-        ]
-      }
-    ]
-    }
-  )
-}
-
-resource "aws_iam_role" "reporting_lambda_role" {
-  name = "ReportingLambdaRole"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        },
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
-  role       = aws_iam_role.reporting_lambda_role.name
-  policy_arn = aws_iam_policy.lambda-policy.arn
-}
-
 output "vpc_id" {
   value = aws_vpc.nb-chatgpt-vpc.id
 }
@@ -77,4 +13,9 @@ output "private_subnet_id" {
 
 output "nat_gateway_public_ip" {
   value = aws_nat_gateway.nb-nat-gw.public_ip
+}
+
+
+output "acces_instance" {
+  value = aws_instance.nb-instance["web-public"].public_ip
 }
--- rds.tf
+++ rds.tf
@@ -15,7 +15,7 @@ resource "aws_db_subnet_group" "nb-db-subnet" {
 resource "aws_security_group" "rds-sg" {
   depends_on  = [aws_subnet.nb-subnet]
   name        = "rds-sg"
-  description = "Security group to allow access rds-subnet from private subnets"
+  description = "Security group to allow access rds-subnet from public subnets"
   vpc_id      = aws_vpc.nb-chatgpt-vpc.id
 
   tags = {
@@ -26,22 +26,23 @@ resource "aws_security_group" "rds-sg" {
 resource "aws_vpc_security_group_ingress_rule" "allow-access-rds" {
   depends_on        = [aws_security_group.rds-sg]
   security_group_id = aws_security_group.rds-sg.id
-  cidr_ipv4         = aws_subnet.nb-subnet["private-net-1"].cidr_block
+  cidr_ipv4         = var.vpc_cidr
   from_port         = 3306
   to_port           = 3306
   ip_protocol       = "tcp"
 }
 
+
 resource "aws_db_instance" "nb-db" {
-  depends_on             = [aws_security_group.rds-sg, aws_vpc_security_group_ingress_rule.allow-access-rds]
-  allocated_storage      = 10
-  db_name                = "nbdb"
-  engine                 = "mysql"
-  instance_class         = "db.t3.micro"
-  username               = var.db_credentials.username
-  password               = var.db_credentials.password
-  publicly_accessible    = false
-  vpc_security_group_ids = [aws_security_group.rds-sg.id]
-  db_subnet_group_name   = aws_db_subnet_group.nb-db-subnet.name
-  skip_final_snapshot    = true
+  depends_on                  = [aws_security_group.rds-sg, aws_vpc_security_group_ingress_rule.allow-access-rds]
+  allocated_storage           = 10
+  db_name                     = "nbdb"
+  engine                      = "mysql"
+  instance_class              = "db.t3.micro"
+  username                    = var.db_credentials.username
+  manage_master_user_password = true
+  publicly_accessible         = false
+  vpc_security_group_ids      = [aws_security_group.rds-sg.id]
+  db_subnet_group_name        = aws_db_subnet_group.nb-db-subnet.name
+  skip_final_snapshot         = true
 }
